e765f0dfd590d9c191ec2f19baa096761c850772
Plauman <petert.lauman@gmail.com>
2026-09-09 13:26:34 -0500

    Add files via upload

---
+++ Lab2/isOdd.c
@@ -0,0 +1,15 @@
+#include "isOdd.h"
+
+#include <stdio.h>
+
+int isOdd(int num)
+{
+    if (num % 2 != 0) {
+        printf("%d is odd.\n", num);
+    }
+    else {
+        printf("%d is even.\n", num);
+    }
+
+    return 0;
+}
